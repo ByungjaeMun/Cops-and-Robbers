51fba517ca16cc6bfde7d808664f99814f1f8b5b
ByungjaeMun <mbj9424@gmail.com>
2016-11-16 19:46:55 +0900

    Initial commit

---
+++ app/src/main/java/car/android/com/cops_and_robber/MainActivity.java
@@ -0,0 +1,13 @@
+package car.android.com.cops_and_robber;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
